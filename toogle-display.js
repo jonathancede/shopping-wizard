1f608904b21b0c308aafa6e6937cd30760eaa592
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-23 11:20:32 +0100

    first implementation of toogle display

---
+++ toogle-display.js
@@ -0,0 +1,25 @@
+var indexCurrentPage = 1;
+
+function CheckAndNextPage(event){
+    switch (indexCurrentPage) {
+        case 1:
+            var mainleftMP = document.getElementById("main-left-mp");
+            var mainRightMP = document.getElementById("main-right-mp");
+            mainRightMP.style.display = "none";
+            mainleftMP.style.display = "none";
+            indexCurrentPage++;
+            break;
+        case 1:
+
+            break;
+        case 1:
+
+            break;
+        case 1:
+
+            break;
+        case 1:
+
+            break;
+    }
+}
