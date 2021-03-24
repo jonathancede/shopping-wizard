ac32930eacc7baea33275d113648ec7b9d928803
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-24 17:15:04 +0100

    Solved appearing/disappearing pop-up window

--- time-window.js
+++ time-window.js
@@ -1,10 +1,10 @@
 // POP-UP TIME WINDOW
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Test
+// Test >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //console.log("Time-window.js loaded");
 
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Window element
+// Window element >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 var timeWindow = document.getElementById("time-window");
 //console.log("Time window:", timeWindow);
 var timeTextElement = document.getElementById("minutes-left");
@@ -12,28 +12,16 @@ var timeTextElement = document.getElementById("minutes-left");
 var windowCross = document.getElementById("time-cross");
 windowCross.addEventListener("click", closeWindow);
 
+var currentTime;
+var fiveMinutes;
+var miliSecondsLeft = new String;
+var secondsLeft;
+var minutesLeft;
+const minute = 1000*60;
+
+
 
 document.getElementById("buy-button").addEventListener("click", initializeTime);
-
-function initializeTime(event){
-    // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Date calculation
-    var currentTime = new Date().getTime();
-    var minute = (1000)*60;
-    var fiveMinutes = currentTime + (minute*5);
-    // Minutes left
-    var miliSecondsLeft = new String;
-    miliSecondsLeft = fiveMinutes-currentTime;
-    // Convert miliseconds into seconds
-    secondsLeft = miliSecondsLeft/1000;
-    // Convert seconds into minutes
-    minutesLeft = secondsLeft/60;
-    // Appearing element
-    timeWindow.style.opacity = "0%";
-    timeWindow.style.display = "flex";
-    timeWindow.style.animation = "timeWindowAnimationIn .4s ease forwards";
-    // Loading remaining minutes text
-    timeTextElement.innerHTML = `You have ${minutesLeft} minutes left`;
-}
 
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Time intervals
@@ -47,11 +35,9 @@ function tenSeconds(){
     // Disappearing element
     timeWindow.style.animation = "timeWindowAnimationOut .4s ease forwards";
     // Setting display to none
-    setInterval(displayNone, 500);
+    setTimeout(displayNone, 500);
 }
 
-// Hiding window after ten seconds
-setTimeout(tenSeconds, 10000);
 
 // Tracking minutes
 function eachMinute(){
@@ -60,24 +46,54 @@ function eachMinute(){
     // Updating string containing minutes
     timeTextElement.innerHTML = `You have ${minutesLeft} minutes left`;
     // Appearing element
+    timeWindow.style.display = "flex";
     timeWindow.style.animation = "timeWindowAnimationIn .4s ease forwards";
     // Hiding window after ten seconds
     setTimeout(tenSeconds, 10000);
 }
 
-// Execute eachMinute function every minute
-setInterval(eachMinute, 20000)
 
-
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Clicking cross function
+// Clicking cross function >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 function closeWindow(event){
     // Disappearing element
     timeWindow.style.animation = "timeWindowAnimationOut .4s ease forwards";
     // Setting display to none
-    setInterval(displayNone, 500);
+    setTimeout(displayNone, 500);
 }
 
 
+// Clicking cross function >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+function subtractSecond(){
+    secondsLeft -= 1;
+    minutesConverted = parseInt(secondsLeft/60);
+    secondsConverted = parseInt(((secondsLeft/60) % 1)*60);
+    console.log(`Current time ${minutesConverted} minutes and ${secondsConverted} seconds.`);
+}
 
 
+function initializeTime(){
+    // Date calculation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    currentTime = new Date().getTime();
+    fiveMinutes = currentTime + (minute*5);
+    // Minutes left
+    miliSecondsLeft = fiveMinutes-currentTime;
+    // Convert miliseconds into seconds
+    secondsLeft = miliSecondsLeft/1000;
+    // Convert seconds into minutes
+    minutesLeft = secondsLeft/60;
+
+    // Appearing element
+    timeWindow.style.opacity = "0%";
+    timeWindow.style.display = "flex";
+    timeWindow.style.animation = "timeWindowAnimationIn .4s ease forwards";
+    timeTextElement.innerHTML = `You have ${minutesLeft} minutes left`;
+
+    // Hiding window after ten seconds
+    setTimeout(tenSeconds, 10000);
+
+    // Execute eachMinute function every minute
+    setInterval(eachMinute, minute);
+
+    // Every second
+    setInterval(subtractSecond, 1000);
+}
