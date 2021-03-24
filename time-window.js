61c67507b58d6ee287ff758af398b77853c88cf3
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-24 12:08:11 +0100

    Implemented getTime()

--- time-window.js
+++ time-window.js
@@ -9,9 +9,13 @@ console.log("Time window:", timeWindow);
 
 
 // Date calculation
-var currentTime = new Date()
+var currentTime = new Date().getTime();
 var minute = (1000)*60;
-var fiveMinutes = currentTime + minute*5;
+var fiveMinutes = currentTime + (minute*5);
 
-console.log(currentTime);
-console.log("Five minutes", fiveMinutes);
+console.log("Current time", currentTime);
+console.log("Five minutes later", fiveMinutes);
+
+
+
+
