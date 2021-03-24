f39a307753f909a8330be95087a570e466dace2d
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-24 11:45:22 +0100

    Added test console.log

---
+++ time-window.js
@@ -0,0 +1,8 @@
+// Pop-up time window functionality
+
+// Test
+console.log("Time-window.js loaded");
+
+// Window element
+let timeWindow = document.getElementById("time-window");
+console.log("Time window:", timeWindow);
