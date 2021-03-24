b7e46d93f42d5976e6ebfad55b07ff3cb4b461c9
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-24 12:01:14 +0100

    corrected error in the declaration of the purchase object

--- toogle-display.js
+++ toogle-display.js
@@ -3,28 +3,28 @@ var indexCurrentPage = 1;       // This variable indicates the index of the curr
 /*---- This is the object where the information will be storaged ----*/
 var purchase = {
     /*---- Global variables to store the product details ----*/
-    typeOfPlant,
-    quantity,
-    typeOfBase,
-    isCustomMessage,
-    customMessage = new String,
+    typeOfPlant: 1,
+    quantity: 1,
+    typeOfBase: 1,
+    isCustomMessage: false,
+    customMessage: new String,
     /*---- Global variables to store the details of Profile ----*/
-    userName = new String,
-    email = new String,
-    password = new String,
+    userName: new String,
+    email: new String,
+    password: new String,
     /*---- Global variables to store the details of Address ----*/
-    firstName = new String,
-    lastName = new String,
-    birthday = new String,
-    addressOne = new String,
-    addressTwo = new String,
-    postalCode = new String,
-    country = new String,
-    phone = new String,
+    firstName: new String,
+    lastName: new String,
+    birthday: new String,
+    addressOne: new String,
+    addressTwo: new String,
+    postalCode: new String,
+    country: new String,
+    phone: new String,
     /*---- Global variables to store the details of Shipping ----*/
-    typeOfShipping,
-    isGift,
-    giftMessage = new String
+    typeOfShipping: 1,
+    isGift: false,
+    giftMessage: new String
 };
 
 function CheckAndNextPage(event){
