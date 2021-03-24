52caf049411d9ee7d53c1eb113292a1ed9214524
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-24 12:16:04 +0100

    Added test time functions

--- time-window.js
+++ time-window.js
@@ -3,6 +3,7 @@
 // Test
 console.log("Time-window.js loaded");
 
+
 // Window element
 var timeWindow = document.getElementById("time-window");
 console.log("Time window:", timeWindow);
@@ -13,9 +14,22 @@ var currentTime = new Date().getTime();
 var minute = (1000)*60;
 var fiveMinutes = currentTime + (minute*5);
 
-console.log("Current time", currentTime);
-console.log("Five minutes later", fiveMinutes);
+//console.log("Current time", currentTime);
+//console.log("Five minutes later", fiveMinutes);
 
 
+// Time intervals
+// Tracking seconds
+function eachSecond(){
+    console.log("Second");
+}
 
+setInterval(eachSecond, 1000)
+
+// Tracking ten seconds
+function tenSeconds(){
+    timeWindow.style.backgroundColor="red";
+}
+
+setTimeout(tenSeconds, 10000);
 
