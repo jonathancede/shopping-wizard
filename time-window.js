fc2551c0bc3abb466741740694c68150fc2ffb66
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-24 12:20:38 +0100

    Added initial hiding function

--- time-window.js
+++ time-window.js
@@ -1,35 +1,38 @@
-// Pop-up time window functionality
+// POP-UP TIME WINDOW
 
-// Test
+// >>>>>>>>>>>>> Test
 console.log("Time-window.js loaded");
 
 
-// Window element
+// >>>>>>>>>>>>> Window element
 var timeWindow = document.getElementById("time-window");
 console.log("Time window:", timeWindow);
 
 
-// Date calculation
+// >>>>>>>>>>>>> Date calculation
 var currentTime = new Date().getTime();
 var minute = (1000)*60;
 var fiveMinutes = currentTime + (minute*5);
+
+var minutesLeft = new String;
+console.log(minutesLeft);
 
 //console.log("Current time", currentTime);
 //console.log("Five minutes later", fiveMinutes);
 
 
-// Time intervals
+// >>>>>>>>>>>>> Time intervals
 // Tracking seconds
 function eachSecond(){
     console.log("Second");
 }
+//setInterval(eachSecond, 1000)
 
-setInterval(eachSecond, 1000)
-
-// Tracking ten seconds
-function tenSeconds(){
-    timeWindow.style.backgroundColor="red";
+// Tracking five seconds
+function fiveSeconds(){
+    // Hide pop-up window after 5 seconds of showing-up
+    timeWindow.style.display="none";
 }
 
-setTimeout(tenSeconds, 10000);
+setTimeout(fiveSeconds, 5000);
 
