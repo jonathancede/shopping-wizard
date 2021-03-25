7d75d36c7d27e290c97e6c326b2053b65d32868b
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-25 10:36:52 +0100

    Added main-price-text.js

---
+++ main-price-text.js
@@ -0,0 +1,11 @@
+// MAIN PAGE PRICE
+
+// Test >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+console.log("Main page price");
+
+
+// Global variables >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+// HTML elements >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
