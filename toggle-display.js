c637ddfd9732273626415886eefa0c707efa7e5a
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-25 17:13:13 +0100

    added the functionality of save data and check password

--- toggle-display.js
+++ toggle-display.js
@@ -30,10 +30,73 @@ var purchase = {
 };
 
 document.getElementById("buy-button").addEventListener("click", StorageDataAndNextPage, true);
-document.getElementById("profile-form").addEventListener("submit", StorageDataAndNextPage, true);
+document.getElementById("profile-form").addEventListener("submit", CheckPassword, true);
 document.getElementById("address-form").addEventListener("submit", StorageDataAndNextPage, true);
 document.getElementById("confirm-button-shipping").addEventListener("click", StorageDataAndNextPage, true);
 
+document.getElementById("back-button-profile").addEventListener("click", BackPage);
+document.getElementById("back-button-address").addEventListener("click", BackPage);
+document.getElementById("back-button-shipping").addEventListener("click", BackPage);
+document.getElementById("back-button-finish").addEventListener("click", BackPage);
+
+function isValid(str){
+    var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return format.test(str);
+}
+
+function CheckPassword(event){
+    event.preventDefault();
+
+    var passwordStr = new String;
+    passwordStr = document.getElementById("user-password").value;
+    var isNumber = false;
+    var isUpper = false;
+    var isLower = false;
+    var isSpecial = false;
+
+    for(i=0 ; i<passwordStr.length ; i++){
+        var number = parseInt(passwordStr[i], 10);
+
+        if(passwordStr[i] == passwordStr[i].toUpperCase()){
+            isUpper = true;
+        }
+        if(passwordStr[i] == passwordStr[i].toLowerCase()){
+            isLower = true;
+        }
+        if(Number.isInteger(number)){
+            isNumber = true;
+        }
+        if(isValid(passwordStr[i])){
+            isSpecial = true;
+        }
+    }
+
+    if((isNumber == true) && (isUpper == true) && (isLower == true) && (isSpecial == true)){
+        var confirmText = document.getElementById("user-password-confirm").value;
+
+        document.getElementById("user-password").style.backgroundColor = "#dcf1eb";
+        document.getElementById("user-password").style.borderLeft = "12px solid #dcf1eb";
+        document.getElementById("user-password").style.color = "#00d5c2";
+
+        if(confirmText == passwordStr){
+            StorageDataAndNextPage(event);
+
+            document.getElementById("user-password-confirm").style.backgroundColor = "#dcf1eb";
+            document.getElementById("user-password-confirm").style.borderLeft = "12px solid #dcf1eb";
+            document.getElementById("user-password-confirm").style.color = "#00d5c2";
+        }
+        else{
+            document.getElementById("user-password-confirm").style.backgroundColor = "#f3d8da";
+            document.getElementById("user-password-confirm").style.borderLeft = "12px solid #f3d8da";
+            document.getElementById("user-password-confirm").style.color = "#d22b41";
+        }
+    }
+    else{
+        document.getElementById("user-password").style.backgroundColor = "#f3d8da";
+        document.getElementById("user-password").style.borderLeft = "12px solid #f3d8da";
+        document.getElementById("user-password").style.color = "#d22b41";
+    }
+}
 
 function StorageDataAndNextPage(event){
     event.preventDefault();
@@ -51,7 +114,7 @@ function StorageDataAndNextPage(event){
             mainRightNextPage.style.display = "flex";
 
             purchase.quantity = document.getElementById("quantity-box").value;
-            if(document.getElementById("custom-message-id").checked == true){
+            if(purchase.isCustomMessage == true){
                 purchase.customMessage = document.getElementById("custom-text").value;
             }
 
@@ -100,15 +163,17 @@ function StorageDataAndNextPage(event){
         case 4:
             var mainleftCurrentPage = document.getElementById("main-left-resume");
             var mainRightCurrentPage = document.getElementById("main-right-shipping");
-
             var mainleftNextPage = document.getElementById("main-left-finish");
             var mainRightNextPage = document.getElementById("main-right-finish");
 
             mainleftCurrentPage.style.display = "none";
             mainRightCurrentPage.style.display = "none";
-
             mainleftNextPage.style.display = "flex";
             mainRightNextPage.style.display = "flex";
+
+            if(purchase.isGift == true){
+                purchase.giftMessage = document.getElementById("gift-text").value;
+            }
             
             indexCurrentPage++;
             break;
