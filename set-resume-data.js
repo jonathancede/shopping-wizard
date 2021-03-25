cf54d3a43a858406119fae486f0cbb434d6678e4
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-25 19:30:39 +0100

    first steps of set-resume-data

--- set-resume-data.js
+++ set-resume-data.js
@@ -1,1 +1,16 @@
-// document.getElementById("").
+document.getElementById("buy-button").addEventListener("click", SetResumeData);
+
+var plantResumeDataElement = document.getElementById("plant-resume-data");
+var baseResumeDataElement = document.getElementById("plant-resume-data");
+var customMessageResumeDataElement = document.getElementById("plant-resume-data");
+
+function SetResumeData(){
+    switch (purchase.typeOfPlant) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
