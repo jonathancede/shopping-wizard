759923e13b5bb217b2da360a1a71f3757f48f22e
Jonathan Cedeño <jony.cede@gmail.com>
2021-03-25 14:46:59 +0100

    rename the file

--- toggle-display.js
+++ toggle-display.js
@@ -21,6 +21,7 @@ var purchase = {
     addressTwo: new String,
     postalCode: new String,
     country: new String,
+    prePhone: new String,
     phone: new String,
     /*---- Global variables to store the details of Shipping ----*/
     typeOfShipping: 1,
@@ -28,54 +29,71 @@ var purchase = {
     giftMessage: new String
 };
 
-// Test print
-console.log(">>>>>>>>>>>>>> Test:", purchase.typeOfPlant);
+document.getElementById("buy-button").addEventListener("click", StorageDataAndNextPage, true);
+document.getElementById("profile-form").addEventListener("submit", StorageDataAndNextPage, true);
+document.getElementById("address-form").addEventListener("submit", StorageDataAndNextPage, true);
+document.getElementById("confirm-button-shipping").addEventListener("click", StorageDataAndNextPage, true);
 
-function CheckAndNextPage(event){
+
+function StorageDataAndNextPage(event){
     event.preventDefault();
+
     switch (indexCurrentPage) {
         case 1:
             var mainleftCurrentPage = document.getElementById("main-left-mp");
             var mainRightCurrentPage = document.getElementById("main-right-mp");
-
             var mainleftNextPage = document.getElementById("main-left-resume");
             var mainRightNextPage = document.getElementById("main-right-profile");
 
             mainleftCurrentPage.style.display = "none";
             mainRightCurrentPage.style.display = "none";
-
             mainleftNextPage.style.display = "flex";
             mainRightNextPage.style.display = "flex";
+
+            purchase.quantity = document.getElementById("quantity-box").value;
+            if(document.getElementById("custom-message-id").checked == true){
+                purchase.customMessage = document.getElementById("custom-text").value;
+            }
 
             indexCurrentPage++;
             break;
         case 2:
             var mainleftCurrentPage = document.getElementById("main-left-resume");
             var mainRightCurrentPage = document.getElementById("main-right-profile");
-
             var mainleftNextPage = document.getElementById("main-left-resume");
             var mainRightNextPage = document.getElementById("main-right-address");
 
             mainleftCurrentPage.style.display = "none";
             mainRightCurrentPage.style.display = "none";
-
             mainleftNextPage.style.display = "flex";
             mainRightNextPage.style.display = "flex";
+
+            purchase.userName = document.getElementById("user-name").value;
+            purchase.email = document.getElementById("user-mail").value;
+            purchase.password = document.getElementById("user-password").value;
             
             indexCurrentPage++;
             break;
         case 3:
             var mainleftCurrentPage = document.getElementById("main-left-resume");
             var mainRightCurrentPage = document.getElementById("main-right-address");
-
             var mainleftNextPage = document.getElementById("main-left-resume");
             var mainRightNextPage = document.getElementById("main-right-shipping");
 
             mainleftCurrentPage.style.display = "none";
             mainRightCurrentPage.style.display = "none";
-
             mainleftNextPage.style.display = "flex";
             mainRightNextPage.style.display = "flex";
+
+            purchase.firstName = document.getElementById("user-first-name").value;
+            purchase.lastName = document.getElementById("user-last-name").value;
+            purchase.birthday = document.getElementById("user-birtdhay").value;
+            purchase.addressOne = document.getElementById("user-address-one").value;
+            purchase.addressTwo = document.getElementById("user-address-two").value;
+            purchase.postalCode = document.getElementById("user-postalcode").value;
+            purchase.country = document.getElementById("user-country").value;
+            purchase.prePhone = document.getElementById("user-phone-prefix").value;
+            purchase.phone = document.getElementById("user-phone").value;
             
             indexCurrentPage++;
             break;
@@ -111,7 +129,7 @@ function CheckAndNextPage(event){
             indexCurrentPage = 1;
             break;
     }
-    console.log(indexCurrentPage);
+    console.log(purchase);
 }
 
 function BackPage(){
@@ -178,5 +196,5 @@ function BackPage(){
             // indexCurrentPage = 1;
             break;
     }
-    console.log(indexCurrentPage);
+    console.log(purchase);
 }
