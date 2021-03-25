2d6a551731d108afa5fdd22f3ba3863de71364c7
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-25 11:27:41 +0100

    Corrected typo and finished switch plant

--- main-price-text.js
+++ main-price-text.js
@@ -12,7 +12,7 @@ let plantWrapper = document.getElementById("type-plant-wrapper");
 let plantName = document.querySelector("#type-plant-wrapper h2");
 let plantNameText = plantName.innerHTML;
 
-let plantPrice = document.getElementById("ttype-plant-total");
+let plantPrice = document.getElementById("type-plant-total");
 let plantPriceText = plantPrice.innerHTML;
 
 document.getElementById("plant1").addEventListener("click", addPlant);
@@ -25,6 +25,9 @@ let baseType = document.querySelector("#type-base-wrapper h2");
 let baseText = baseType.innerHTML;
 
 
+// Quantity
+
+
 
 // Functions >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // Total
@@ -33,15 +36,39 @@ function addTotal(){
 
 // Plant
 function addPlant(){
-    // Show element
-    plantWrapper.style.display = "block";
-    // Replace plant name
-    plantName.innerHTML = purchase.typeOfPlant;
-    // Replace price
-    plantPrice.innerHTML = purchase.
+    console.log("test >>>>>>>>>>>>>>>>>>");
 
-    console.log("Plant added");
-
+    // Adapt plant price
+    switch(purchase.typeOfPlant){
+        // Var
+        case 1:
+            // Replace plant name
+            plantName.innerHTML = "Var";
+            // Replace price
+            plantPrice.innerHTML = "10.00€";
+            break;
+        // Tri
+        case 2:
+            // Replace plant name
+            plantName.innerHTML = "Tri";
+            // Replace price
+            plantPrice.innerHTML = "12.00€";
+            break;
+        // Cam
+        case 3:
+            // Replace plant name
+            plantName.innerHTML = "Cam";
+            // Replace price
+            plantPrice.innerHTML = "14.00€";
+            break;
+        // Scap
+        case 4:
+            // Replace plant name
+            plantName.innerHTML = "Scap";
+            // Replace price
+            plantPrice.innerHTML = "16.00€";
+            break;
+    }
 }
 
 // Base
