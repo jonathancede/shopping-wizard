2ba8d37ee419cb82ed70a7cad1de11a5bb948ed2
Ricard Garcia <imricardgarcia@gmail.com>
2021-03-25 14:15:02 +0100

    Constructed function to switch back to main

--- time-window.js
+++ time-window.js
@@ -10,7 +10,7 @@ var timeWindow = document.getElementById("time-window");
 //console.log("Time window:", timeWindow);
 var timeTextElement = document.getElementById("minutes-left");
 var timeTextTitle = document.querySelector("#time-window-text h2");
-console.log(timeTextTitle.innerHTML);
+// console.log(timeTextTitle.innerHTML);
 //console.log("Minutes left:", timeTextElement);
 var windowCross = document.getElementById("time-cross");
 windowCross.addEventListener("click", closeWindow);
@@ -36,7 +36,7 @@ document.getElementById("back-to-main-page").addEventListener("click", resetTime
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Time intervals
 // Half second to display none
 function displayNone(){
-    timeWindow.style.display= "none";
+    timeWindow.style.display = "none";
 }
 
 // Tracking ten seconds
@@ -47,6 +47,59 @@ function tenSeconds(){
     setTimeout(displayNone, 500);
 }
 
+// Back to main page
+function backToMain(){
+    // Clearing all intervals
+    resetTime();
+
+    // Setting current page to main page
+    switch (indexCurrentPage) {
+        case 1:
+            var mainleftCurrentPage = document.getElementById("main-left-mp");
+            var mainRightCurrentPage = document.getElementById("main-right-mp");
+
+            mainleftCurrentPage.style.display = "none";
+            mainRightCurrentPage.style.display = "none";
+            break;
+        case 2:
+            var mainleftCurrentPage = document.getElementById("main-left-resume");
+            var mainRightCurrentPage = document.getElementById("main-right-profile");
+
+            mainleftCurrentPage.style.display = "none";
+            mainRightCurrentPage.style.display = "none";
+            break;
+        case 3:
+            var mainleftCurrentPage = document.getElementById("main-left-resume");
+            var mainRightCurrentPage = document.getElementById("main-right-address");
+
+            mainleftCurrentPage.style.display = "none";
+            mainRightCurrentPage.style.display = "none";
+            break;
+        case 4:
+            var mainleftCurrentPage = document.getElementById("main-left-resume");
+            var mainRightCurrentPage = document.getElementById("main-right-shipping");
+
+            mainleftCurrentPage.style.display = "none";
+            mainRightCurrentPage.style.display = "none";
+            break;
+        case 5:
+            var mainleftCurrentPage = document.getElementById("main-left-finish");
+            var mainRightCurrentPage = document.getElementById("main-right-finish");
+
+            mainleftCurrentPage.style.display = "none";
+            mainRightCurrentPage.style.display = "none";
+            break;
+    }
+
+    // Showing back the main page
+    var mainleftCurrentPage = document.getElementById("main-left-mp");
+    var mainRightCurrentPage = document.getElementById("main-right-mp");
+    mainleftCurrentPage.style.display = "flex";
+    mainRightCurrentPage.style.display = "flex";
+
+    // Reset index page
+    indexCurrentPage = 1;
+}
 
 // Tracking minutes
 function eachMinute(){
@@ -77,7 +130,23 @@ function subtractSecond(){
     minutesConverted = parseInt(secondsLeft/60);
     secondsConverted = parseInt(((secondsLeft/60) % 1)*60);
     remainingTime = `Current time ${minutesConverted} minutes and ${secondsConverted} seconds.`
-    // console.log(remainingTime);
+    
+    // Go back to main page if there's no time left
+    if (secondsLeft==0) {
+        // Updating string in window
+        timeTextTitle.innerHTML = "You have exceeded 5 minutes";
+        timeTextElement.innerHTML = "Going back to main page";
+        // Appearing pop-up window
+        timeWindow.style.display = "flex";
+        timeWindow.style.animation = "timeWindowAnimationIn .4s ease forwards";
+        // Hiding pop-up window after ten seconds
+        setTimeout(tenSeconds, 10000);
+        // Hiding pop-up window after ten seconds
+        setTimeout(backToMain, 10000);
+    }
+
+    // Remaining minutes and seconds
+    console.log(remainingTime);
 }
 
 // Executed when user clicks "back" to main page
@@ -101,9 +170,9 @@ function resetTime(){
 
 // Executed when user clicks "buy" at main page
 function initializeTime(){
-    // Date calculation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
     currentTime = new Date().getTime();
-    fiveMinutes = currentTime + (minute*5);
+    fiveMinutes = currentTime + (minute*2);
     // Minutes left
     miliSecondsLeft = fiveMinutes-currentTime;
     // Convert miliseconds into seconds
@@ -128,11 +197,3 @@ function initializeTime(){
 }
 
 
-if(miliSecondsLeft==0){
-    // Updating string containing minutes
-    timeTextElement.innerHTML = `Going back to main page`;
-    // Appearing pop-up window
-    timeWindow.style.display = "flex";
-    timeWindow.style.animation = "timeWindowAnimationIn .4s ease forwards";
-
-}
